8affe369d13eb65d8beea4548750b8fb2f571e44
Abdelhamide EL ARIB <elarib.abdelhamide@gmail.com>
2016-06-07 02:02:15 +0000

    Solve the First & Second question Week1 assignement

--- Week 1/recfun/src/main/scala/recfun/Main.scala
+++ Week 1/recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -13,15 +15,47 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+
+    def pascal(c: Int, r: Int): Int = {
+      if(c ==0 || c ==r ) 1
+      else {
+        pascal(c-1,r-1) +  pascal(c,r-1)
+      }
+
+  }
+
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+
+      def checkIt(chars: List[Char], numOpenedPar: Int): Boolean = {
+
+        chars match {
+          case Nil => numOpenedPar == 0
+          case h :: tail =>
+            val newNumOpenedPar =
+              if (h == '(')
+                numOpenedPar + 1
+              else if (h == ')')
+                numOpenedPar - 1
+              else
+                numOpenedPar
+            if (newNumOpenedPar > 0 || newNumOpenedPar == 0)
+              checkIt(tail, newNumOpenedPar)
+            else
+              false
+
+          case _ => false
+        }
+
+      }
+      checkIt(chars, 0)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = 0
   }
